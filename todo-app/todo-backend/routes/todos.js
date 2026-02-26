8a431377231d1d1940dced3865c74e3a066cba58
Nyukaa <annashitikova@gmail.com>
2026-02-26 14:25:54 +0200

    12.7 is done

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,49 +1,80 @@
-const express = require('express');
-const { Todo } = require('../mongo')
+const express = require("express");
+const { Todo } = require("../mongo");
 const router = express.Router();
 
 /* GET todos listing. */
-router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
+router.get("/", async (_, res) => {
+  const todos = await Todo.find({});
   res.send(todos);
 });
 
 /* POST todo to listing. */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
-  })
+    done: false,
+  });
   res.send(todo);
 });
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
+  const { id } = req.params;
+  req.todo = await Todo.findById(id);
+  if (!req.todo) return res.sendStatus(404);
 
-  next()
-}
+  next();
+};
 
 /* DELETE todo. */
-singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+singleRouter.delete("/", async (req, res) => {
+  await req.todo.delete();
   res.sendStatus(200);
 });
 
 /* GET todo. */
-singleRouter.get('/', async (req, res) => {
+singleRouter.get("/", async (req, res) => {
   res.sendStatus(405); // Implement this
 });
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
+singleRouter.put("/", async (req, res) => {
   res.sendStatus(405); // Implement this
 });
+//12.7
+// GET one todo
+router.get("/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
 
-router.use('/:id', findByIdMiddleware, singleRouter)
+    if (!todo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
 
+    res.json(todo);
+  } catch (error) {
+    res.status(400).json({ error: "Malformatted id" });
+  }
+});
+// UPDATE one todo
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // Return the updated document
+      runValidators: true, // Mongoose uses schema validation when update
+      context: "query", // Needed for some validators to work properly during update
+    });
+
+    if (!updatedTodo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+
+    res.json(updatedTodo);
+  } catch (error) {
+    res.status(400).json({ error: "Malformatted id" });
+  }
+});
+router.use("/:id", findByIdMiddleware, singleRouter);
 
 module.exports = router;
